2a08157f9d1aa621125243fea5b827d00d24d210
lilingling <1192246772@qq.com>
2017-08-07 21:15:37 +0800

    'local'

---
+++ src/components/foodDetail/foodDetail.vue
@@ -0,0 +1,36 @@
+<template>
+    <transition name="move">
+      <div class="detailWrapper" ref="detailWrapper" v-show="showDetail">
+        <div class="foodDetail">
+          <div class="back" @click="showToggle()">
+            <i class="icon-arrow_lift"></i>
+          </div>
+        </div>
+      </div>
+    </transition>
+</template>
+
+<script>
+export default {
+  props:['food'],
+  data(){
+    return {
+      showDetail: false,
+    }
+  },
+  components:{},
+  computed:{},
+  methods:{
+    showToggle(){
+      this.showDetail = !this.showDetail;
+      
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
+
+
